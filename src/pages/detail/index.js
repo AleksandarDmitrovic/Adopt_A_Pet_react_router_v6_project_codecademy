d83b21020d153e7a37b592850d495afe2f1b3e30
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-06-04 14:29:14 -0600

    Add Navigate react router cmpt to redirect in error on loading pet details

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getPetDetails } from "../../api/petfinder";
 import Hero from "../../components/hero";
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 
 // Import useParams
 // Import Navigate
@@ -10,7 +10,7 @@ const PetDetailsPage = () => {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  const { id } = useParams(); // <--- Update me!
+  const { id } = useParams();
 
   useEffect(() => {
     async function getPetsData() {
@@ -33,7 +33,10 @@ const PetDetailsPage = () => {
         <h3>Loading...</h3>
       ) : error ? (
         <div>
-          {/* Redirect to /pet-details-not-found if there was an error! */}
+          {
+            /* Redirect to /pet-details-not-found if there was an error! */
+            <Navigate to="/pet-details-not-found" />
+          }
         </div>
       ) : (
         <main>
