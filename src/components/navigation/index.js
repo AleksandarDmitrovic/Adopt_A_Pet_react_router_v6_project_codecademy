481a680910954d59d782b70aa6fb2699b1da393a
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2025-06-04 13:30:42 -0600

    Fix add NavLink to property

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -27,8 +27,7 @@ const Navigation = () => {
       <ul className="nav-links">
         <li key={"all"}>
           {/* These links should be NavLink component and add a special active class name if its an active link */}
-          <NavLink href="/" className="nav-link">
-            {" "}
+          <NavLink to="/" className="nav-link">
             All Pets
           </NavLink>
         </li>
@@ -37,7 +36,7 @@ const Navigation = () => {
               <li key={type.name}>
                 {/* These links should be NavLink component and add a special active class name if its an active link */}
                 <NavLink
-                  href={`/${type._links.self.href.split("/").pop()}`}
+                  to={`/${type._links.self.href.split("/").pop()}`}
                   key={type.name}
                   className="nav-link"
                 >
